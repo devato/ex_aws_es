4d34f71afdcd0fbae8d46f3d2bc44e6b0aa4f85d
troy.martin <troy.martin@riskrecon.com>
2021-04-23 08:41:18 -0400

    Init

--- mix.exs
+++ mix.exs
@@ -1,18 +1,18 @@
-defmodule ExAwsElasticSearch.MixProject do
+defmodule ExAwsES.MixProject do
   use Mix.Project
 
   @version "1.0.1"
 
   def project do
     [
-      app: :ex_aws_elasticsearch,
+      app: :ex_aws_es,
       version: @version,
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      source_url: "https://github.com/JustinTangg/ex_aws_elasticsearch",
-      homepage_url: "https://github.com/JustinTangg/ex_aws_elasticsearch",
+      source_url: "https://github.com/devato/ex_aws_es",
+      homepage_url: "https://github.com/devato/ex_aws_es",
       package: package(),
       docs: [
         main: "readme",
@@ -39,16 +39,16 @@ defmodule ExAwsElasticSearch.MixProject do
       {:hackney, "1.6.3 or 1.6.5 or 1.7.1 or 1.8.6 or ~> 1.9", only: [:dev, :test]},
       {:poison, ">= 1.2.0", optional: true},
       {:ex_doc, "~> 0.19.2", only: [:dev, :test]},
-      {:ex_aws, "~> 2.0"}
+      {:ex_aws, "~> 2.1"}
     ]
   end
 
   defp package do
     [
       description: "AWS Elasticsearch service for ex_aws",
-      maintainers: ["Justin Tang"],
+      maintainers: ["Troy Martin"],
       licenses: ["MIT"],
-      links: %{github: "https://github.com/JustinTangg/ex_aws_elasticsearch"}
+      links: %{github: "https://github.com/devato/ex_aws_es"}
     ]
   end
 end
